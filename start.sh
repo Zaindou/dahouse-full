aa7cffbab2d524af901ed5bb87ce10f8fdca6544
Douglas Anaya <102629422+Zaindou@users.noreply.github.com>
2025-01-24 17:30:49 -0500

    Add start.sh for pm2 ecosystem.
    Add start.sh for pm2 ecosystem.

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /var/www/dahouse-full/env/bin/activate
+exec gunicorn wsgi:app \
+    --bind 0.0.0.0:8000 \
+    --workers 4 \
+    --threads 2 \
+    --worker-class=gthread \
+    --worker-connections=1000 \
+    --keep-alive 5 \
+    --max-requests 1000 \
+    --max-requests-jitter 50 \
+    --timeout 30
