197c867352a91a6c4c3856cac1a899de7ca980c1
Douglas Anaya <102629422+Zaindou@users.noreply.github.com>
2025-01-24 20:36:02 -0500

    jupdate
    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,22 +9,31 @@ module.exports = {
       env: {
         NODE_ENV: "production",
       },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
     {
       name: "Backend",
       script: "/var/www/dahouse-full/start.sh",
-      cwd: "/var/www/dahouse-full"
-    }
+      cwd: "/var/www/dahouse-full",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
   ],
   deploy: {
     production: {
       user: "dahouse", // Usuario SSH
-      host: "152.201.100.105", // IP del servidor
+      host: "vnc.dahouse.co", // IP del servidor
       ref: "origin/main", // Rama principal
       repo: "git@github.com:Zaindou/dahouse-full.git", // Repositorio
       path: "/var/www/dahouse-full", // Ruta de despliegue
       "post-deploy":
-        "yarn install --cwd ./frontend && yarn build --cwd ./frontend && pm2 reload ecosystem.config.js --env production",
+        "yarn install --cwd ./frontend && yarn build --cwd ./frontend && pm2 reload ecosystem.config.cjs --env production",
     },
   },
 };
